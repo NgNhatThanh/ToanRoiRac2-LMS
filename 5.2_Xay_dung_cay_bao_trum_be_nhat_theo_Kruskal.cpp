17fc1d8e386ee4eb51fbce6ed5a5f66c385c37d5
NgNhatThanh <thanhtobot159@gmail.com>
2024-04-17 11:15:14 +0700

    fix kruskal

--- 5.2_Xay_dung_cay_bao_trum_be_nhat_theo_Kruskal.cpp
+++ 5.2_Xay_dung_cay_bao_trum_be_nhat_theo_Kruskal.cpp
@@ -6,7 +6,20 @@
 #define se second
 using namespace std;
 
-int n, mtKe[14][14], vst[14];
+int n, mtKe[14][14], par[14];
+
+int find_par(int u){
+    if(par[u] == u) return u;
+    return par[u] = find_par(par[u]);
+}
+
+int join(int u, int v){
+    u = find_par(u);
+    v = find_par(v);
+    if(u == v) return 0;
+    par[u] = v;
+    return 1;
+}
 
 void solve(){
     vector<ppi> v;
@@ -18,11 +31,12 @@ void solve(){
     sort(begin(v), end(v));
     int res = 0;
     vector<pair<int, int>> canh;
+
     for(auto x : v){
-        if(vst[x.se.fi] && vst[x.se.se]) continue;
-        res += x.fi;
-        canh.push_back({x.se.fi, x.se.se});
-        vst[x.se.fi] = vst[x.se.se] = 1;
+        if(join(x.se.fi, x.se.se)){
+            res += mtKe[x.se.fi][x.se.se];
+            canh.push_back({min(x.se.fi, x.se.se), max(x.se.fi, x.se.se)});
+        }
     }
     cout << "dH = " << res << '\n';
     for(auto x : canh) cout << x.fi << ' ' << x.se << '\n';
@@ -31,6 +45,7 @@ void solve(){
 int main(){
     cin >> n;
     for(int i = 1; i <= n; ++i){
+        par[i] = i;
         for(int j = 1; j <= n; ++j) cin >> mtKe[i][j];
     }
     solve();
